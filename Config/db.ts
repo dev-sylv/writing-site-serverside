2a1742fbbe8617430eb3476cde14cd456ca26d1d
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 16:48:32 -0800

    saved db connection

---
+++ Config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const lifeURI = ""
+
+const db_Url = "mongodb://localhost/AugustineBlog"
+
+export const DBconnect = async() =>{
+    try {
+        const connectionstring = await mongoose.connect(db_Url);
+        console.log(`Database is connected to server on ${connectionstring}`)
+    } catch (error) {
+        
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import express, { Application, Request, Response} from "express";
 import {environmentVariables} from "../Config/environmentVariables"
 
 import cors from "cors";
+import { DBconnect } from "../Config/db";
 
 const port = environmentVariables.port
 
 // Instantiating my server called app from express
 const app: Application = express();
+
+// Connecting my database to server:
+DBconnect()
 
 // setting up needed middlewares for my application
 app.use(express.json());
@@ -15,7 +19,9 @@ app.use(cors());
 
 // Setting up my default url for app:
 app.get("/", (req: Request, res: Response) =>{
-    console.log("Server is up and running for the writing site")
+    return res.status(200).json({
+        message: "Server is up and running for the writing site"
+    })
 })
 
 // Listening to my port
