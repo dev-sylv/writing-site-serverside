42a88215bc86c36e531f5cdcaec80ef82a260a54
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 16:51:32 -0800

    saved all

--- Config/db.ts
+++ Config/db.ts
@@ -7,8 +7,8 @@ const db_Url = "mongodb://localhost/AugustineBlog"
 export const DBconnect = async() =>{
     try {
         const connectionstring = await mongoose.connect(db_Url);
-        console.log(`Database is connected to server on ${connectionstring}`)
+        console.log(`Database is connected to server on ${connectionstring.connection.host}`)
     } catch (error) {
-        
+        console.log(`Couldn't connect to ${db_Url}`)
     }
 }
