6762e30391d4388c3c0ed8af77e58d53e8ff2373
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 22:00:25 -0800

    saved single blog post

--- Routes/BlogRoutes.ts
+++ Routes/BlogRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { AllBlogPost } from "../Controllers/BlogController";
+import { AllBlogPost, SingleBlogPost } from "../Controllers/BlogController";
 
 const blogRoutes = Router();
 
 blogRoutes.route("/all-blog-posts").get(AllBlogPost);
+blogRoutes.route("/single-blog-posts/:blogID").get(SingleBlogPost);
 
 export default blogRoutes
