d678367702fcda5630bbf60b709ac6ef11d9a091
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 21:50:28 -0800

    saved all blog post

--- Controllers/BlogController.ts
+++ Controllers/BlogController.ts
@@ -3,7 +3,27 @@ import BlogModels from "../Models/postModels";
 import { Request, Response } from "express";
 
 // Get all blog posts:
+export const AllBlogPost = async(req: Request, res: Response): Promise<Response> =>{
+    try {
+        const blogposts = await BlogModels.find();
 
+        if (!blogposts) {
+            return res.status(404).json({
+                message: "There are no blog posts"
+            })
+        } else {
+            return res.status(200).json({
+                message: "Successfully got all blog posts",
+                data: blogposts
+            })
+        }
+    } catch (error) {
+        return res.status(400).json({
+            message: "Couldn't get all blog posts",
+            data: error
+        })
+    }
+}
 
 // Get a single post:
 
