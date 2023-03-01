6ee308f8a725fd54b1dc856496012eafc6eff347
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 17:15:09 -0800

    saved all

--- Config/db.ts
+++ Config/db.ts
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-const lifeURI = "mongodb+srv://sylviaDB:atlaspassword@cluster0.fhx2vt1.mongodb.net/AugustineBlog"
+const LifeURI = "mongodb+srv://sylviaDB:atlaspassword@cluster0.fhx2vt1.mongodb.net/AugustineBlog?retryWrites=true&w=majority"
 
 const db_Url = "mongodb://localhost/AugustineBlog"
 
 export const DBconnect = async() =>{
     try {
-        const connectionstring = await mongoose.connect(lifeURI);
+        const connectionstring = await mongoose.connect(db_Url);
+        console.log("")
         console.log(`Database is connected to server on ${connectionstring.connection.host}`)
     } catch (error) {
-        console.log(`Couldn't connect to ${db_Url}`)
+        console.log(`Couldn't connect to ${LifeURI}`)
     }
 }
