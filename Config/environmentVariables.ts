12661f6b31754ab4edb538046661a4c2be77ff21
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 18:52:27 -0800

    saved updates

--- Config/environmentVariables.ts
+++ Config/environmentVariables.ts
@@ -3,5 +3,8 @@ import dotenv from "dotenv"
 dotenv.config();
 
 export const environmentVariables = {
-    port: process.env.port as string
+    port: process.env.port as string,
+    Adminname: process.env.name as string,
+    AdminEmail: process.env.email as string,
+    AdminPassword: process.env.password as string,
 }
