ba1ab423f47b05d2222b688ebd0a77b94f7f3fc3
nicsylvia <nicsylvia15f@gmail.com>
2023-02-28 16:41:55 -0800

    saved server

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// Setting up my default url for app:
+app.get("/", (req: Request, res: Response) =>{
+    console.log("Server is up and running for the writing site")
+})
+
 // Listening to my port
 app.listen(port, () =>{
+    console.log("")
     console.log("Listening to server on port", port)
 })
